e8fdda66fcda2be3d002fc256abd2dcb2b55edc1
Rohan Mathur <rohan@rmathur.com>
2016-05-19 11:24:55 -0400

    Add the secondary ClassFactory class to generate models on the fly

---
+++ create.py
@@ -0,0 +1,10 @@
+def ClassFactory(name, argnames, BaseClass=BaseClass):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if key not in argnames:
+                raise TypeError("Argument %s not valid for %s"
+                    % (key, self.__class__.__name__))
+            setattr(self, key, value)
+        BaseClass.__init__(self, name[:-len("Class")])
+    newclass = type(name, (BaseClass,),{"__init__": __init__})
+    return newclass
